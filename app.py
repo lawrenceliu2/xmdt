a29a3c2f40fb1050add107b110d61b7755b6f408
Lawrence Liu <lawrence255@yahoo.com>
2016-11-04 14:09:39 -0400

    MEMES

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Xtreme Meme Dream Team
-import os
+import os, util.db_manager
 from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
@@ -25,29 +25,19 @@ def disp_register():
 
 @app.route('/auth', methods=["POST"])
 def authenticate():
-    if request.form["username"]=="" and request.form["pass"]=="":#gotta get users info from database, FORTUNES JOB
+    if db_manager.userAuth(request.form["username"], request.form["pass"]):
         session["username"] = request.form["username"]
         return render_template("home.html", message="youre in my dude")
     else:
-        if request.form["username"]=="":#gotta get users info from database, FORTUNES JOB
-            return render_template("login.html", message="wrong password my dude, try again")
-        elif request.form["pass"]=="":#gotta get users info from database, FORTUNES JOB
-            return render_template("login.html", message="wrong username my dude, try again")
-        else:
-            return render_template("login.html", message="no account with those credentials my dude, try again")
+        return render_template("home.html", message="you dun gofed my dude")
 
-        
-@app.route('/registration', methods=["POST"])
-def registration():
-    if request.form["username"]=="":#find the user in the users.db, FORTUNES JOB
-        return render_template("register.html", message="that username is taken my dude, try again")
-    elif request.form["pass"] != request.form["pass-conf"]:
-        return render_template("register.html", message="passwords dont match my dude, try again")
+@app.route('/rauthgister', methods=["POST"])
+def auth_register():
+    if db_manager.addUser(request.form["username"], request.form["pass"]) == 1:
+        return render_template("home.html")
     else:
-        return render_template("login.html", message="account successfully created my dude, login now")
-            
-
-                
+        return render_template("register.html")
+    
 @app.route('/home')
 def disp_stories():
     return render_template('home.html')
