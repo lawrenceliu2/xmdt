0f4375bc027c5ec25dfe8e4e4e3a5b941644039b
Fortune Soleil <fsoleil@stuy.edu>
2016-11-06 17:44:54 -0500

    Update db_manager.py

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -75,12 +75,12 @@ def getID(username):
      x = users.fetchall()
      return x[0][0]
 
-def hasContributed(username):
+def hasContributed(title, username):
      x = getID(username)
      sdb = sqlite3.connect("data/stories.db")
      stories = sdb.cursor()
 
-     q = "SELECT * FROM users WHERE userid=%s;" % (x)
+     q = "SELECT * FROM " + title + " WHERE userid=%s;" % (x)
      stories.execute(q)
      info = stories.fetchall()
      if (len(info) > 0): return True
